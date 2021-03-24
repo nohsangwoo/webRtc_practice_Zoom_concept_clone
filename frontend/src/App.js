38f0cd4762b60526ba85e15ad31ab1781fba0b0d
nohsangwoo <nsgr12@naver.com>
2021-03-24 11:14:27 +0900

    add comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import AssignmentIcon from '@material-ui/icons/Assignment';
 import PhoneIcon from '@material-ui/icons/Phone';
 import React, { useEffect, useRef, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+// front-end단 peer
 import Peer from 'simple-peer';
+// front-end단 socket.io
 import io from 'socket.io-client';
 import './App.css';
 
@@ -25,8 +27,9 @@ function App() {
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
-
   useEffect(() => {
+    //  socket.io api에서 webRtc api를 지원하는듯
+    // webRtc API를 이용하여 사용자의 비디오를 불러옴
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then(stream => {
@@ -34,7 +37,15 @@ function App() {
         myVideo.current.srcObject = stream;
       });
 
+    // 뱍엔드 서버에서 "me"라는 트리거를 이용하여 id라는 변수인자와 함께 front-end쪽으로 보냄
+    // "me"트리거에 반응하는 FE쪽 함수
     socket.on('me', id => {
+      //BE에서 보낸 socket.id를 프론트단의 setMe useState에 저장
+      console.log(
+        '첫번째 동작: 사용자 socket.id(고유 아이디)를 setMe(사용자 고유 아이디) 해줌',
+        id
+      );
+
       setMe(id);
     });
 
@@ -46,6 +57,9 @@ function App() {
     });
   }, []);
 
+  // 제일 먼저 callUset를 작동시키는 함수
+  //   callUse의 root임
+  //  상대방의 아이디로 상대방과 접속함
   const callUser = id => {
     const peer = new Peer({
       initiator: true,
@@ -127,9 +141,17 @@ function App() {
             label="Name"
             variant="filled"
             value={name}
-            onChange={e => setName(e.target.value)}
+            onChange={e => {
+              console.log(
+                '두번째 동작: 자신의 이름을 입력하면 setName에 이름을 저장(onChange)',
+                name
+              );
+              return setName(e.target.value);
+            }}
             style={{ marginBottom: '20px' }}
           />
+          {/* CopyToClipboard 라이브러리를 이용하여 생성된 id를 복사함 */}
+          {/* 사용자 고유아이디(socket.io)를 복사해줌 */}
           <CopyToClipboard text={me} style={{ marginBottom: '2rem' }}>
             <Button
               variant="contained"
@@ -140,6 +162,8 @@ function App() {
             </Button>
           </CopyToClipboard>
 
+          {/* 접속할 대상의 상대방 고유 아이디(socket.id)를 입력하면 setIdToCall에 저장*/}
+          {/*callUser버튼을 누르면 calluser함수가 작동하는데 이때 전달되는 인자가 idToCall */}
           <TextField
             id="filled-basic"
             label="ID to call"
@@ -148,11 +172,16 @@ function App() {
             onChange={e => setIdToCall(e.target.value)}
           />
           <div className="call-button">
+            {/* callAccepted가 true이고 동시에 callEnded가 false일때만 endCall버튼이 활성화 */}
             {callAccepted && !callEnded ? (
               <Button variant="contained" color="secondary" onClick={leaveCall}>
                 End Call
               </Button>
             ) : (
+              // 이버튼을 누르면 해당 아이디로 callUser를 작동시킨다
+              //callUser버튼을 누르면 calluser함수가 작동하는데 이때 전달되는 인자가 idToCall
+              // 방만드는 방식을 상대방이 고유 아이디로 만들지말고 uuid를 이용하여 고유 방을 만든다음
+              // 상대방이 고유 방번호로 접속가능하게 구성하는것도 괜찮을듯
               <IconButton
                 color="primary"
                 aria-label="call"
--- server.js
+++ server.js
@@ -2,21 +2,29 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+//socket.io config
+// 이 노드서버를 socket.io화 시키고
 const io = require('socket.io')(server, {
+  // cors설정도 해줌
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
 
+//사용자가 localhost:3000접속시 FE단으로 날리는 함수
 io.on('connection', socket => {
+  //사용자가 localhost:3000으로 접속하면 socket.io 고유 아이디를 "me"트리거를 이용하여 FE쪽으로 보냄
   socket.emit('me', socket.id);
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('callEnded');
   });
 
+  //   FE단에서 먼저 "callUser" 트리거로 신호를 보내줘야 작동함
+  //  FE단에서 보낸 변수와 함께 작동
   socket.on('callUser', data => {
+    //   아마도 FE단에서 데이터를 받으면 데이터를 가공하여 다시 FE로 보내는 함수인듯
     io.to(data.userToCall).emit('callUser', {
       signal: data.signalData,
       from: data.from,
